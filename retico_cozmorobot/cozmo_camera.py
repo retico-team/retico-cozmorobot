685500b884956e5b5c74e1745df24466f78acaba
Slim Lab User <josuetorresfonse@u.boisestate.edu>
2024-02-16 11:00:08 -0700

    camera is working better now

--- retico_cozmorobot/cozmo_camera.py
+++ retico_cozmorobot/cozmo_camera.py
@@ -39,7 +39,7 @@ class CozmoCameraModule(retico_core.AbstractProducingModule):
     def output_iu():
         return ImageIU
 
-    def __init__(self, robot, exposure=0.05, gain=0.1, **kwargs):
+    def __init__(self, robot, exposure=0.2, gain=0.1, **kwargs):
         # for exp room:exposure=0.05, gain=0.05
         super().__init__(**kwargs)
         self.robot = robot
@@ -49,26 +49,22 @@ class CozmoCameraModule(retico_core.AbstractProducingModule):
         self.img_queue = deque(maxlen=1)
         
     def process_update(self, blank):
-
         if len(self.img_queue) > 0:
             img = self.img_queue.popleft()
             # img = np.array(img)
+            # print('using image')
             output_iu = self.create_iu(None)
             output_iu.set_image(img, 1, 1)
-            return retico_core.UpdateMessage.from_iu(output_iu, retico_core.UpdateType.ADD)
-
+            self.append(retico_core.UpdateMessage.from_iu(output_iu, retico_core.UpdateType.ADD))
+        else:
+            # this hack keeps the camera running semi-smoothly, otherwise it gets clogged
+            # self.robot.camera.image_stream_enabled = False
+            time.sleep(0.9)
+            self.configure_camera()
     
-    # def process_iu(self, input_iu):
-    #     if len(self.img_queue) > 0:
-    #         img = self.img_queue.popleft()
-    #         # img = np.array(img)
-    #         output_iu = self.create_iu(input_iu)
-    #         output_iu.set_image(img, 1, 1)
-    #         return output_iu
-
-    #     return None
 
     def configure_camera(self):
+        self.robot.camera.image_stream_enabled = True
         self.robot.camera.color_image_enabled = True
         self.robot.camera.enable_auto_exposure = False # False means we can adjust manually
         # Lerp exposure between min and max times
@@ -82,11 +78,12 @@ class CozmoCameraModule(retico_core.AbstractProducingModule):
         self.robot.camera.set_manual_exposure(exposure_time,actual_gain)
 
     def setup(self):
-        
         def handle_image(evt, obj=None, tap_count=None,  **kwargs):
             self.img_queue.append(evt.image)
 
+        # print("configuring camera")
         self.configure_camera()
+        # print('adding event handler')
         self.robot.world.add_event_handler(cozmo.camera.EvtNewRawCameraImage, handle_image)
 
 
